28017d7fd41ffe2f9d249418edc332359cff13cb
Jonas-lopez <Jonas-lopez@users.noreply.github.com>
2014-05-09 01:12:14 -0500

    Create Index.php
    Este es el archivo principal del sistema en construcción.

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Reporte de Clientes</title>
+	<script type="text/javascript" language="javascript" src="js/jquery-1.11.1.js"></script>
+	<script type="text/javascript" language="javascript" src="js/jquery.cycle.js"></script>
+	<script type="text/javascript" language="javascript" src="js/configuracion.js"></script>    
+	<style type="text/css" rel="stylesheet" media="all">@import"css/General.css";</style>
+</head>
+	<body>
+	
+  <?php
+  
+      echo ("Hola Mundo");
+  
+  ?>
+		
+	</body>
+</html>
