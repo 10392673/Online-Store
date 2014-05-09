79de6ddfafad59ed339216958647c34263b5d353
Jonas-lopez <Jonas-lopez@users.noreply.github.com>
2014-05-09 01:26:42 -0500

    Create conector.php

---
+++ _lib/php/conector.php
@@ -0,0 +1,19 @@
+<?php 
+
+	@$conexion = new mysqli();
+	@$conexion->connect("localhost", "root", "", "facturas");
+
+if (@$conexion->connect_error) 
+{
+    die('Error de conexión: ' . @$conexion->connect_error); 
+}
+
+/*  else{	
+	
+	echo ("conexion exitoso");
+} */
+
+	#@$conexion->close();	
+
+?>
+
