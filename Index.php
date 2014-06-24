3af49b8de57395c14f266ea79fc5da72b34fb41f
Jonas <Jonas-lopez@users.noreply.github.com>
2014-06-24 01:38:01 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,19 +2,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Reporte de Clientes</title>
-	<script type="text/javascript" language="javascript" src="_lib/js/jquery-1.11.1.js"></script>
+	<meta name="author" content="Autor: TSU Alan Jiménez Quirós, Jonas Santiz Lopez, Crhistian Argenis Catzin Cach, Uriel Amir Barragán Pereira, Universidad Tecnologica de Cancun">
+	<title>P&aacute;gina en construcci&oacute;n </title>
+	<script type="text/javascript" language="javascript" src="_lib/js/jquery-1.10.2.js"></script>
 	<script type="text/javascript" language="javascript" src="_lib/js/jquery.cycle.js"></script>
-	<script type="text/javascript" language="javascript" src="_lib/js/config.js"></script>    
-	<style type="text/css" rel="stylesheet" media="all">@import"_lib/css/General.css";</style>
+	<script type="text/javascript" language="javascript" src="_lib/js/config.js"></script>
+	<!--<style type="text/css" rel="stylesheet" media="screen">@import"css/bootstrap-3.1.1/css/bootstrap.css";</style>-->
+	<style type="text/css" rel="stylesheet" media="screen">@import"_lib/css/general.css";</style>
 </head>
 	<body>
+
+		<h1>P&aacute;gina en construcci&oacute;n</h1>
+		<br />
+		 <div class="clock">
+			<div id="Date"></div>
+			<ul>
+				<li id="hours"> </li>
+				<li id="point">:</li>
+				<li id="min"> </li>
+				<li id="point">:</li>
+				<li id="sec"> </li>
+			</ul>
+		</div>
 	
-  <?php
-  
-      echo ("Hola Mundo ke hace?");
-  
-  ?>
-		
 	</body>
 </html>
