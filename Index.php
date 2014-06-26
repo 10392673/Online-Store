aed8b449c95e054b54dd9882d4523c3e4f0a09b5
Jonas <Jonas-lopez@users.noreply.github.com>
2014-06-26 18:59:27 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,18 +2,51 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="author" content="Autor: TSU Alan Jiménez Quirós, Jonas Santiz Lopez, Crhistian Argenis Catzin Cach, Uriel Amir Barragán Pereira, Universidad Tecnologica de Cancun">
+	<meta name="author" content="Autor: TSU Alan Jim&eacute;nez Quir&oacute;s, Jonas Santiz Lopez, Crhistian Argenis Catzin Cach, Uriel Amir Barragán Pereira, Universidad Tecnologica de Cancun">
 	<title>P&aacute;gina en construcci&oacute;n </title>
 	<script type="text/javascript" language="javascript" src="_lib/js/jquery-1.10.2.js"></script>
 	<script type="text/javascript" language="javascript" src="_lib/js/jquery.cycle.js"></script>
+	<script type="text/javascript" language="javascript" src="_lib/js/jquery.fancybox.pack.js"></script>
 	<script type="text/javascript" language="javascript" src="_lib/js/config.js"></script>
 	<!--<style type="text/css" rel="stylesheet" media="screen">@import"css/bootstrap-3.1.1/css/bootstrap.css";</style>-->
+	<style type="text/css" rel="stylesheet" media="screen">@import"_lib/css/jquery.fancybox.css";</style>
 	<style type="text/css" rel="stylesheet" media="screen">@import"_lib/css/general.css";</style>
 </head>
 	<body>
-
-		<h1>P&aacute;gina en construcci&oacute;n</h1>
-		<br />
+	
+	
+	
+	<div class="wrapper" >
+		<div class="header">
+			<a class="logoh" href="javascript:void(0);"></a>			
+			<div class="menu1" >
+				<ul>
+					<li class="activ" ><a href="javascript:void(0);">Inicio</a></li>
+					<li><a href="javascript:void(0);">Facturas</a></li>
+					<li><a href="javascript:void(0);">Articulos</a></li>
+					<li><a href="javascript:void(0);">Stock</a></li>
+					<li><input type="text" name="buscar" class="buscar" placeholder="Buscar" /></li>
+					<li><a id="box" data-fancybox-type="iframe" href="_lib/php/loggin.php">Iniciar Sesion</a></li>
+				</ul>
+			</div>
+			<p><a href="skype:alanjkun?call">Contactenos 01 800 522 687</a></p>
+		</div>
+		<div class="left" >
+			<div class="menu2" >
+				<ul>
+					<li><a href="javascript:void(0);">Ofertas y Promociones</a></li>
+					<li><a href="javascript:void(0);">Preguntas Frecuentes</a></li>
+					<li><a href="javascript:void(0);">Quejas y Sugerencias</a></li>
+					<li><a href="javascript:void(0);">Ubicacion</a></li>
+					<li><a href="javascript:void(0);">Noticias</a></li>
+				</ul>
+			</div>
+			<img src="_lib/img/pc.png" alt="promo" />
+			<p>A meses sin intereses</p>
+		</div>
+		<p class="sep" >/Inicio</p>
+		<div class="content" >
+			<h1>Pagina en Construccion</h1>
 		 <div class="clock">
 			<div id="Date"></div>
 			<ul>
@@ -23,7 +56,26 @@
 				<li id="point">:</li>
 				<li id="sec"> </li>
 			</ul>
+		</div>	
+		
 		</div>
+		<div class="fotter" >
+			<ul>
+				<li><a href="#">Quienes somos?</a></li>
+				<li><a href="#">Acerca de</a></li>
+				<li><a href="#">Mapa del sitio</a></li>
+				<li><a href="#">Politica de privacidad</a></li>
+			</ul>
+			<p>
+				&copy;2014 Online . Todos los Derechos Reservados, el uso de este sitio constituye un acuerdo en
+	los T&eacute;rminos de Uso. Consulte las pol&iacute;ticas. Se proh&iacute;be cualquier reproducci&oacute;n 
+		total o	parcial de este contenido sin autorizaci&oacute;n por 
+					escrito de su titular.
+			</p>
+		</div>
+	</div>
 	
+	
+
 	</body>
 </html>
