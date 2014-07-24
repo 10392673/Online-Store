8108ada6288b6fe84e78e24ddfbb7b647cdf5b65
Jonas <thimixsadecv@hotmail.com>
2014-07-24 12:52:53 -0500

    Actualizacion de archivos

--- _lib/php/lap.php
+++ _lib/php/lap.php
@@ -1,212 +1,208 @@
-      <style type="text/css" rel="stylesheet" media="screen">@import"../css/general.css";</style>
-
 		<div class="pc" >
-		<img class="banner" src="../img/pc8.jpg" />
 			<h2>Laptop disponibles</h2>
+		<img class="banner" src="_lib/img/pc8.jpg" />
 			<div class="promopc">
-					<img src="../img/dell1.jpg" alt="" />
+					<img src="_lib/img/dell1.jpg" alt="" />
 				<div class="art" >
-					<p>DELL DC560, 5 GB RAM, 650 GB DDR, i6</p>
+					<p>DELL, 5 GB RAM, 650 GB HD, i6</p>
 					<b>$8,650</b>
 					<p>10 Meses sin ineterese</p>
-					<label>Pagos desde $652</label>
+					<label>Pagos desde $865</label>
 					<b>Clave del art: #1130</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/pc.png" alt="" />
+					<img src="_lib/img/pc.png" alt="" />
 				<div class="art" >
-					<p>SAMSUNG SQL 4 GB RAM, 480 GB DDR, i5</p>
+					<p>SAMSUNG 4 GB RAM, 480 GB HD, i5</p>
 					<b>$9,560</b>
 					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $692.5</label>
+					<label>Pagos desde $1,195</label>
 					<b>Clave del art: #1131</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/pc5.png" alt="" />
+					<img src="_lib/img/pc5.png" alt="" />
 				<div class="art" >
-					<p>TOSHIBA, 6GB RAM 750 GB DDR, i7</p>
+					<p>TOSHIBA, 6GB RAM 750 GB HD, i7</p>
 					<b>$7,600</b>
-					<p>10 Meses sin ineterese</p>
-					<label>Pagos desde $487</label>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $950</label>
 					<b>Clave del art: #1132</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/sony2.png" alt="" />
+					<img src="_lib/img/sony2.png" alt="" />
 				<div class="art" >
-					<p>SONY VAIO, 4GB RAM, 560 GB DDR, i5</p>
+					<p>SONY VAIO, 4GB RAM, 560 GB HD, i5</p>
 					<b>$8,860</b>
 					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $1062.5</label>
+					<label>Pagos desde $1,107.5</label>
 					<b>Clave del art: #1133</b>
 				</div>
 			</div>	
 
 			<div class="promopc">
-					<img src="../img/lap1.png" alt="" />
+					<img src="_lib/img/lap1.png" alt="" />
 				<div class="art" >
-					<p>ACER QRS, 5GB RAM, 600GB DDR, i7</p>
-					<b>$8,600</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $1,075</label>
+					<p>Lenovo, 4GB RAM, 540GB HD, i5</p>
+					<b>$9,400</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $940</label>
 					<b>Clave del art: #1134</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap2.jpg" alt="" />
+					<img src="_lib/img/lap2.jpg" alt="" />
 				<div class="art" >
-					<p>DELL GRT, 4GB RAM, 550 GB DDR, i8</p>
-					<b>$7,560</b>
-					<p>10 Meses sin ineterese</p>
-					<label>Pagos desde $756</label>
+					<p>Azus, 3GB RAM, 650 GB HD, i6</p>
+					<b>$9,820</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,227.5</label>
 					<b>Clave del art: #1135</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap3.jpg" alt="" />
+					<img src="_lib/img/lap3.jpg" alt="" />
 				<div class="art" >
-					<p>HP CQ560, 5GB RAM, 600 GB DDR, i8</p>
-					<b>$10,500</b>
-					<p>12 Meses sin ineterese</p>
-					<label>Pagos desde $875 </label>
+					<p>SONNY 4GB RAM, 540 GB HD, i4</p>
+					<b>$7,800</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $780</label>
 					<b>Clave del art: #1136</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap4.png" alt="" />
+					<img src="_lib/img/lap4.png" alt="" />
 				<div class="art" >
-					<p>MAC MC516xx/A 17,3"/D2,4G/2x1GB/250/SD-DL</p>
-					<b>$14,580</b>
-					<p>12 Meses sin ineterese</p>
-					<label>Pagos desde $1,215</label>
+					<p>HP 3GB RAM, 650 GB DH, i5</p>
+					<b>$9,640</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,205</label>
 					<b>Clave del art: #1137</b>
 				</div>
 			</div>
 			
 			<div class="promopc">
-					<img src="../img/lap5.jpg" alt="" />
+					<img src="_lib/img/lap5.jpg" alt="" />
 				<div class="art" >
-					<p>HP CQ700, 6GB RAM, 650GB DDR, i6</p>
-					<b>$7,569</b>
-					<p>9 Meses sin ineterese</p>
-					<label>Pagos desde $841</label>
+					<p>TOSHIBA, 5 GB RAM, 500GB DH, i5</p>
+					<b>$8,600</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,075</label>
 					<b>Clave del art: #1138</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap6.jpg" alt="" />
+					<img src="_lib/img/lap6.jpg" alt="" />
 				<div class="art" >
-					<p>DELL TRS, 5GB RAM, 520 GB DDR, i7 </p>
-					<b>$5,540</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $692.5</label>
+					<p>SONNY, 4GB RAM, 420 GB HD, i3 </p>
+					<b>$8,699</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $869.9</label>
 					<b>Clave del art: #1139</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap7.jpg" alt="" />
+					<img src="_lib/img/lap7.jpg" alt="" />
 				<div class="art" >
-					<p>HP CQ400, 5GB GB RAM, 650 GB DDR, i4</p>
-					<b>$5,520</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $690</label>
+					<p>SAMSUNG, 6GB GB RAM, 750 GB HD, i5</p>
+					<b>$10,520</b>
+					<p>11 Meses sin ineterese</p>
+					<label>Pagos desde $956.3</label>
 					<b>Clave del art: #1140</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap8.jpg" alt="" />
+					<img src="_lib/img/lap8.jpg" alt="" />
 				<div class="art" >
-					<p>LENOVO IDEAPAD, 7 GB RAM, 750 GB DDR, i7</p>
-					<b>$9,850</b>
+					<p>AZUS, 4 GB RAM, 550 GB HD, i5</p>
+					<b>$9,560</b>
 					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $985</label>
+					<label>Pagos desde $1,195</label>
 					<b>Clave del art: #1141</b>
 				</div>
 			</div>
 
 			<div class="promopc">
-					<img src="../img/lap9.jpg" alt="" />
+					<img src="_lib/img/lap9.jpg" alt="" />
 				<div class="art" >
-					<p>HP CQ580, 5 GB RAM, 450 DDR, i6</p>
-					<b>$7,560</b>
-					<p>10 Meses sin ineterese</p>
-					<label>Pagos desde $756</label>
+					<p>DELL, 4 GB RAM, 520 HD, i5</p>
+					<b>$8,600</b>
+					<p>9 Meses sin ineterese</p>
+					<label>Pagos desde $955.5</label>
 					<b>Clave del art: #1142</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap10.jpg" alt="" />
+					<img src="_lib/img/lap10.jpg" alt="" />
 				<div class="art" >
-					<p>DELL RTF, 7 GB RAM, 850 GB DDR, i6</p>
-					<b>$9,870</b>
+					<p>MAC MC516xx/A 16,5"/D2,6G/2x1GB</p>
+					<b>$13,986</b>
 					<p>12 Meses sin ineterese</p>
-					<label>Pagos desde $822.5</label>
+					<label>Pagos desde $1,165.5</label>
 					<b>Clave del art: #1143</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap11.jpg" alt="" />
+					<img src="_lib/img/lap11.jpg" alt="" />
 				<div class="art" >
-					<p>MAC MC516xx/A 18,5"/D2,6G/2x1GB</p>
-					<b>$14,870</b>
-					<p>10 Meses sin ineterese</p>
-					<label>Pagos desde $1,487</label>
+					<p>LENOVO, 5 GB RAM, 620 HD, i7</p>
+					<b>$10,564</b>
+					<p>9 Meses sin ineterese</p>
+					<label>Pagos desde $1,173.7</label>
 					<b>Clave del art: #1144</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap12.jpg" alt="" />
+					<img src="_lib/img/lap12.jpg" alt="" />
 				<div class="art" >
-					<p>SONNY VAIO DSD, 5 GB RAM, 540 GB DDR, i8</p>
-					<b>$8,500</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $1062.5</label>
+					<p>ACER, 5 GB RAM, 650 GB HD, i7</p>
+					<b>$11,260</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $1,126</label>
 					<b>Clave del art: #1145</b>
 				</div>
 			</div>			
 
 			<div class="promopc">
-					<img src="../img/lap13.jpg" alt="" />
+					<img src="_lib/img/lap13.jpg" alt="" />
 				<div class="art" >
-					<p>DELL DSA, 4 GB RAM, 45GB DDR, i4</p>
-					<b>$5,520</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $690</label>
+					<p>COMPAQ, 5 GB RAM, 650GB HD, i5</p>
+					<b>$8,560</b>
+					<p>11 Meses sin ineterese</p>
+					<label>Pagos desde $778.1</label>
 					<b>Clave del art: #1146</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap14.png" alt="" />
+					<img src="_lib/img/lap14.png" alt="" />
 				<div class="art" >
-					<p>COMPAQ CQ459 6GB RAM, 760 DDR, i8</p>
-					<b>$9,860</b>
-					<p>10 Meses sin ineterese</p>
-					<label>Pagos desde $986</label>
+					<p>SONNY 6GB RAM, 660 HD, i7</p>
+					<b>$10,620</b>
+					<p>12 Meses sin ineterese</p>
+					<label>Pagos desde $885</label>
 					<b>Clave del art: #1147</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap15.png" alt="" />
+					<img src="_lib/img/lap15.png" alt="" />
 				<div class="art" >
-					<p>ACER ECS, 4 RAM 550 DDR, i5</p>
-					<b>$6,520</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $815</label>
+					<p>AZUS, 7 RAM 750 HD, i7</p>
+					<b>$11,980</b>
+					<p>12 Meses sin ineterese</p>
+					<label>Pagos desde $998.3</label>
 					<b>Clave del art: #1148</b>
 				</div>
 			</div>
 			<div class="promopc">
-					<img src="../img/lap116.png" alt="" />
+					<img src="_lib/img/lap116.png" alt="" />
 				<div class="art" >
-					<p>HP TDE, 4GB RAM, 800 GB DDR, i6</p>
-					<b>$7,420</b>
-					<p>8 Meses sin ineterese</p>
-					<label>Pagos desde $927.5</label>
+					<p>COMPAQ, 5GB RAM, 600 GB HD, i5</p>
+					<b>$8,630</b>
+					<p>9 Meses sin ineterese</p>
+					<label>Pagos desde $958.8</label>
 					<b>Clave del art: #1149</b>
 				</div>
-			</div>
-			
+			</div>			
 		</div>
-		
