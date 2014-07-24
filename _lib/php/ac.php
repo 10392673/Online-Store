dea14598a8c88fc34e04736a27c35b0459b682df
Jonas <thimixsadecv@hotmail.com>
2014-07-24 14:40:10 -0500

    Actualizacion de Recursos

--- _lib/php/ac.php
+++ _lib/php/ac.php
@@ -1,7 +1,210 @@
+      <style type="text/css" rel="stylesheet" media="screen">@import"../css/general.css";</style>
+	  
+		<div class="pc" >
+			<h2>Laptop disponibles</h2>
+		<img class="banner" src="../img/ac22.jpg" />
+			<div class="promopc">
+					<img src="../img/ac1.jpg" alt="" />
+				<div class="art" >
+					<p>DELL, 5 GB RAM, 650 GB HD, i6</p>
+					<b>$8,650</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $865</label>
+					<b>Clave del art: #1130</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac2.jpg" alt="" />
+				<div class="art" >
+					<p>SAMSUNG 4 GB RAM, 480 GB HD, i5</p>
+					<b>$9,560</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,195</label>
+					<b>Clave del art: #1131</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac3.jpg" alt="" />
+				<div class="art" >
+					<p>TOSHIBA, 6GB RAM 750 GB HD, i7</p>
+					<b>$7,600</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $950</label>
+					<b>Clave del art: #1132</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac4.jpg" alt="" />
+				<div class="art" >
+					<p>SONY VAIO, 4GB RAM, 560 GB HD, i5</p>
+					<b>$8,860</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,107.5</label>
+					<b>Clave del art: #1133</b>
+				</div>
+			</div>	
 
+			<div class="promopc">
+					<img src="../img/ac5.jpg" alt="" />
+				<div class="art" >
+					<p>Lenovo, 4GB RAM, 540GB HD, i5</p>
+					<b>$9,400</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $940</label>
+					<b>Clave del art: #1134</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac6.png" alt="" />
+				<div class="art" >
+					<p>Azus, 3GB RAM, 650 GB HD, i6</p>
+					<b>$9,820</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,227.5</label>
+					<b>Clave del art: #1135</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac7.jpg" alt="" />
+				<div class="art" >
+					<p>SONNY 4GB RAM, 540 GB HD, i4</p>
+					<b>$7,800</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $780</label>
+					<b>Clave del art: #1136</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac8.jpg" alt="" />
+				<div class="art" >
+					<p>HP 3GB RAM, 650 GB DH, i5</p>
+					<b>$9,640</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,205</label>
+					<b>Clave del art: #1137</b>
+				</div>
+			</div>
+			
+			<div class="promopc">
+					<img src="../img/ac9.jpg" alt="" />
+				<div class="art" >
+					<p>TOSHIBA, 5 GB RAM, 500GB DH, i5</p>
+					<b>$8,600</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,075</label>
+					<b>Clave del art: #1138</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac10.jpg" alt="" />
+				<div class="art" >
+					<p>SONNY, 4GB RAM, 420 GB HD, i3 </p>
+					<b>$8,699</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $869.9</label>
+					<b>Clave del art: #1139</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac11.jpg" alt="" />
+				<div class="art" >
+					<p>SAMSUNG, 6GB GB RAM, 750 GB HD, i5</p>
+					<b>$10,520</b>
+					<p>11 Meses sin ineterese</p>
+					<label>Pagos desde $956.3</label>
+					<b>Clave del art: #1140</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac12.png" alt="" />
+				<div class="art" >
+					<p>AZUS, 4 GB RAM, 550 GB HD, i5</p>
+					<b>$9,560</b>
+					<p>8 Meses sin ineterese</p>
+					<label>Pagos desde $1,195</label>
+					<b>Clave del art: #1141</b>
+				</div>
+			</div>
 
-<?php
+			<div class="promopc">
+					<img src="../img/ac13.jpg" alt="" />
+				<div class="art" >
+					<p>DELL, 4 GB RAM, 520 HD, i5</p>
+					<b>$8,600</b>
+					<p>9 Meses sin ineterese</p>
+					<label>Pagos desde $955.5</label>
+					<b>Clave del art: #1142</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac14.jpg" alt="" />
+				<div class="art" >
+					<p>MAC MC516xx/A 16,5"/D2,6G/2x1GB</p>
+					<b>$13,986</b>
+					<p>12 Meses sin ineterese</p>
+					<label>Pagos desde $1,165.5</label>
+					<b>Clave del art: #1143</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac15.jpg" alt="" />
+				<div class="art" >
+					<p>LENOVO, 5 GB RAM, 620 HD, i7</p>
+					<b>$10,564</b>
+					<p>9 Meses sin ineterese</p>
+					<label>Pagos desde $1,173.7</label>
+					<b>Clave del art: #1144</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac16.jpg" alt="" />
+				<div class="art" >
+					<p>ACER, 5 GB RAM, 650 GB HD, i7</p>
+					<b>$11,260</b>
+					<p>10 Meses sin ineterese</p>
+					<label>Pagos desde $1,126</label>
+					<b>Clave del art: #1145</b>
+				</div>
+			</div>			
 
-	echo ("esta es la pagina de accesorios");
-
-?>
+			<div class="promopc">
+					<img src="../img/ac17.jpg" alt="" />
+				<div class="art" >
+					<p>COMPAQ, 5 GB RAM, 650GB HD, i5</p>
+					<b>$8,560</b>
+					<p>11 Meses sin ineterese</p>
+					<label>Pagos desde $778.1</label>
+					<b>Clave del art: #1146</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac18.jpg" alt="" />
+				<div class="art" >
+					<p>SONNY 6GB RAM, 660 HD, i7</p>
+					<b>$10,620</b>
+					<p>12 Meses sin ineterese</p>
+					<label>Pagos desde $885</label>
+					<b>Clave del art: #1147</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac19.jpg" alt="" />
+				<div class="art" >
+					<p>AZUS, 7 RAM 750 HD, i7</p>
+					<b>$11,980</b>
+					<p>12 Meses sin ineterese</p>
+					<label>Pagos desde $998.3</label>
+					<b>Clave del art: #1148</b>
+				</div>
+			</div>
+			<div class="promopc">
+					<img src="../img/ac20.jpg" alt="" />
+				<div class="art" >
+					<p>COMPAQ, 5GB RAM, 600 GB HD, i5</p>
+					<b>$8,630</b>
+					<p>9 Meses sin ineterese</p>
+					<label>Pagos desde $958.8</label>
+					<b>Clave del art: #1149</b>
+				</div>
+			</div>			
+		</div>
