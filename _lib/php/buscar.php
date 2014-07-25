287bacc484a718496a00d2e0d96917a65c8a44a1
Jonas <thimixsadecv@hotmail.com>
2014-07-25 13:12:13 -0500

    Actualizacion de archivos del proyecto.

--- _lib/php/buscar.php
+++ _lib/php/buscar.php
@@ -1,5 +1,14 @@
 	<style type="text/css" rel="stylesheet" media="screen">@import"../css/general.css";</style>
+
 		<div class="buscar" >
 		
+		
+			<table>
+				<tr class="titulo" ><td>Clave</td><td>Producto</td><td>Precio</td><td>Existencia</td><td>Imagen</td><td>Tipo de producto</td></tr>
+				<tr class="show" ><td>#001</td><td>Laptop Lenovo</td><td>$8,890</td><td>3</td><td><img src="../img/b1.png" alt="Producto" /></td><td>Laptop</td></tr>
+			</table>
+			
+			
+			
 		</div>
 		
