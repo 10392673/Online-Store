c9f5ff0cb8677e1a708a800229a227f394983eb1
Jonas <thimixsadecv@hotmail.com>
2014-07-25 12:17:07 -0500

    Actualizaciones

--- _lib/php/buscar.php
+++ _lib/php/buscar.php
@@ -1,45 +1,5 @@
 	<style type="text/css" rel="stylesheet" media="screen">@import"../css/general.css";</style>
-		<div class="pc" >
-			<h2>Articulos en venta</h2>
-			<div class="promopc">
-					<img src="_lib/img/pc1.gif" alt="" />
-				<div class="art" >
-					<p>computadora 1 3 gb ram</p>
-					<b>$5,000</b>
-					<p>10 meses sin ineterese</p>
-					<label>pagos de de 670</label>
-					<a class="btn" href=""></a>
-				</div>
-			</div>
-			<div class="promopc">
-					<img src="_lib/img/pc1.gif" alt="" />
-				<div class="art" >
-					<p>computadora 1 3 gb ram</p>
-					<b>$5,000</b>
-					<p>10 meses sin ineterese</p>
-					<label>pagos de de 670</label>
-					<a class="btn" href=""></a>
-				</div>
-			</div>
-			<div class="promopc">
-					<img src="_lib/img/pc1.gif" alt="" />
-				<div class="art" >
-					<p>computadora 1 3 gb ram</p>
-					<b>$5,000</b>
-					<p>10 meses sin ineterese</p>
-					<label>pagos de de 670</label>
-					<a class="btn" href=""></a>
-				</div>
-			</div>
-			<div class="promopc">
-					<img src="_lib/img/pc1.gif" alt="" />
-				<div class="art" >
-					<p>computadora 1 3 gb ram</p>
-					<b>$5,000</b>
-					<p>10 meses sin ineterese</p>
-					<label>pagos de de 670</label>
-					<a class="btn" href=""></a>
-				</div>
-			</div>			
+		<div class="buscar" >
+		
 		</div>
 		
